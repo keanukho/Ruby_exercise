bd8de4d0e5896397d30f17b1f2cbb971eade2b8f
keanukho <keanukho66@gmail.com>
2024-03-07 15:28:13 +0800

    Calculate Sum

--- simple_ruby/exercise3.rb
+++ simple_ruby/exercise3.rb
@@ -1,5 +1,9 @@
-#Write a Ruby program that takes two numbers as user input, calculates their sum, and prints the result.
+# Write a Ruby program that takes two numbers as user input,
+# calculates their sum, and prints the result.
 
+
+#METHOD TYPE
+#
 def calculate_sum (x,y)
   x+y
 end
@@ -10,3 +14,14 @@ y = 10
 sum = calculate_sum(x, y)
 
 p "The sum of #{x} and #{y} is: #{sum}"
+
+# #SCRIPT TYPE
+#
+# print 'Input a number: '
+#   x = gets.chomp.to_i
+# print 'Input a number: '
+#   y = gets.chomp.to_i
+#
+#   sum = x+y
+# p "The sum of #{x} and #{y} is: #{sum}"
+
