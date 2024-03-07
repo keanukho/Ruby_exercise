76ac5c1279c301a95ce4974db4a1af7902f80c41
keanukho <keanukho66@gmail.com>
2024-03-07 15:32:09 +0800

    Celsius to Fahrenheit

--- simple_ruby/exercise7.rb
+++ simple_ruby/exercise7.rb
@@ -1,18 +1,22 @@
 #Write a Ruby program that takes a temperature in Celsius as input, converts it to Fahrenheit, and prints the result.
 
+#METHOD TYPE
+#
 def celsius_to_fahrenheit(celsius)
   celsius * 9/5 + 32
 end
 
 print 'Input a number: '
-celsius = gets.chomp.to_i
+celsius = gets.chomp.to_f
 fahrenheit = celsius_to_fahrenheit(celsius)
 
 puts "Temperature in Fahrenheit: #{fahrenheit}°F"
 
 
-print 'Input a celsius: '
-var = gets.chomp.to_i
-fahrenheit = var * 9/5 + 32
-
-puts "Temperature in Fahrenheit: #{fahrenheit}"
+# #SCRIPT TYPE
+#
+# print 'Input a celsius: '
+# var = gets.chomp.to_f
+# fahrenheit = var * 9/5 + 32
+#
+# puts "Temperature in Fahrenheit: #{fahrenheit}"
