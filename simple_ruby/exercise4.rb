ccc4720d9d8a971fa9a8d32a3a4c8cbf4035a468
keanukho <keanukho66@gmail.com>
2024-03-07 15:29:14 +0800

    Get name

--- simple_ruby/exercise4.rb
+++ simple_ruby/exercise4.rb
@@ -1,13 +1,29 @@
-#Write a Ruby program that takes a user's first name and last name as input, concatenates them, and prints the full name.
+# Write a Ruby program that takes a user's first name and last name as input,
+# concatenates them, and prints the full name.
 
 
-
+#METHOD TYPE
+#
 def get_name_from_user(first_name, last_name)
-  full_name = first_name<< last_name
+  full_name = first_name + " " + last_name
   "Your full name is: #{full_name}"
-
 end
 
-p get_name_from_user("Keanu", " Kho")
+print 'Input a first name: '
+  first_name = gets.chomp
+print 'Input a last name: '
+  last_name = gets.chomp
+
+puts get_name_from_user(first_name, last_name)
 
 
+# #SCRIPT TYPE
+
+# print 'Input a first name: '
+#   first_name = gets.chomp
+# print 'Input a last name: '
+#   last_name = gets.chomp
+#
+#  full_name =  first_name<< " " << last_name
+#
+# print "your full name is #{full_name}"
