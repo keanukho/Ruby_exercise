e2e5c920e0601634dcbb45b5b6a7f5b5d805cebd
keanukho <keanukho66@gmail.com>
2024-03-07 15:33:39 +0800

    Leap Year

--- simple_ruby/exercise9.rb
+++ simple_ruby/exercise9.rb
@@ -1,11 +1,35 @@
 #Write a Ruby program that takes a year as input and checks whether it is a leap year. Print the result.
 
-def knows_leap_year(year)
-  if year % 4 == 0
-    return "leap year"
-  else
-    return "not leap year"
-  end
-end
+#METHOD TYPE
+#
+# def knows_leap_year(year)
+#   if year % 400 == 0
+#     return "leap year"
+#   elsif year % 100 == 0
+#     return "not leap year"
+#   elsif year % 4 == 0
+#     return "leap year"
+#   else
+#     return "not leap year"
+#   end
+# end
+#
+# print 'Input a year: '
+# year = gets.chomp.to_i
+# puts knows_leap_year(year)
 
-p knows_leap_year(2004)
+
+#SCRIPT TYPE
+
+print 'Input a year: '
+  year = gets.chomp.to_i
+
+  if year % 400 == 0
+    puts "#{year} is a leap year"
+  elsif year % 100 == 0
+    puts "#{year} is not a leap year"
+  elsif year % 4 == 0
+    puts "#{year} is a leap year"
+  else
+    puts "#{year} is not a leap year"
+  end
