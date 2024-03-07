602ffe21ca48dca38d6625b21bf4310182e70819
keanukho <keanukho66@gmail.com>
2024-03-07 15:31:08 +0800

    Swap

--- simple_ruby/exercise6.rb
+++ simple_ruby/exercise6.rb
@@ -1,7 +1,7 @@
 #Write a Ruby program that takes a number as input, determines
 #whether it's even or odd, and prints the result.
 
-
+#METHOD TYPE
 def is_even_or_odd(number)
   if number % 2 == 0
     result = "even"
@@ -13,4 +13,17 @@ end
 
 print 'Input a number: '
 num = gets.chomp.to_i
-p is_even_or_odd(num)
+print is_even_or_odd(num)
+
+
+# #SCRIPT TYPE
+# print 'Input a number: '
+# number = gets.chomp.to_i
+#
+# if number % 2 == 0
+#   result = "even"
+# else
+#   result = "odd"
+# end
+#
+# puts "The number #{number} is #{result}."
