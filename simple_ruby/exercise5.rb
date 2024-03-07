7c0485fc0e4a2aa6b2af70d886ad85d94cdb4bcf
keanukho <keanukho66@gmail.com>
2024-03-07 15:30:37 +0800

    Calculate Area

--- simple_ruby/exercise5.rb
+++ simple_ruby/exercise5.rb
@@ -1,9 +1,26 @@
-#Write a Ruby program that takes the length and width of a rectangle as variables, calculates the area, and prints the result.
+#Write a Ruby program that takes the length and width of a rectangle as variables,
+# calculates the area, and prints the result.
 
+#METHOD TYPE
 def calculate_area(length, width)
   area = length * width
   "The area of the rectangle with length #{length}cm and width #{width}cm is: #{area}"
-
 end
+print "Input a number: "
+  length = gets.chomp.to_i
+print "Input a number: "
+  width = gets.chomp.to_i
 
-p calculate_area(42, 21)
+p calculate_area(length, width)
+
+
+# #SCRIPT TYPE
+#
+# print "Input a number: "
+#   length = gets.chomp.to_i
+# print "Input a number: "
+#   width = gets.chomp.to_i
+#
+#   area = length * width
+#
+# puts "The area of the rectangle with length #{length}cm and width #{width}cm is: #{area}"
