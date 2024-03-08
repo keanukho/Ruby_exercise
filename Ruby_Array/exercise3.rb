9d7c3956ec01e34eed65095721200b8d114476b5
keanukho <keanukho66@gmail.com>
2024-03-08 17:17:14 +0800

    complete this

--- Ruby_Array/exercise3.rb
+++ Ruby_Array/exercise3.rb
@@ -1,4 +1,20 @@
+# Jeanne Keanu Echevarria
+# Class Batch_6
 # Develop a Ruby program that receives an array of ages and counts how many
 # individuals fall into each age group: "Child" (12 or younger), "Teenager" (13 to 19), and "Adult" (20 or older).
 
-#
+
+
+
+
+
+
+new_daily_temperature = daily_temperature.map do |temperature|
+  if temperature > 35
+    "High"
+  elsif temperature > 20 && temperature <= 35
+    "Moderate"
+  else
+    "Low"
+  end
+end
